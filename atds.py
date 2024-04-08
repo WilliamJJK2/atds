18ae178fe599adab983d62a16799f1d6cdde641c
William Kim <wkim25@students.polytechnic.org>
2024-04-08 14:59:02 -0700

    Added BinaryTree!

--- atds.py
+++ atds.py
@@ -71,6 +71,96 @@ class Queue():
             return True
         else:
             return False
+        
+class Deque(object):
+    
+    def __init__(self):
+        self.dequeue = []
+
+    def add_front(self, item):
+        self.dequeue.add(0, item)
+
+    def add_rear(self, item):
+        self.dequeue.append(item)
+
+    def remove_front(self):
+        return self.dequeue.pop(0)
+    
+    def remove_rear(self):
+        return self.dequeue.pop()
+    
+    def size(self):
+        return len(self.dequeue)
+    
+    def is_empty(self):
+        return len(self.dequeue) == 0
+    
+#!/usr/bin/env python3
+"""
+atds.py
+The module containing all of the data structures used in the
+Advanced Topics in CS course at Poly.
+"""
+__author__ = "Richard White"
+__version__ = "2024-02-13"
+
+
+class Stack(object):
+    """Describes a Stack that can be used with push
+    and pop commands.
+    """
+    def __init__(self):
+        self.st = []
+
+    def push(self, item):
+        self.st.append(item)
+
+    def pop(self):
+        if len(self.st) > 0:
+            return self.st.pop()
+        else:
+            return None
+    
+    def peek(self):
+        """Returns the item on the "top" of
+        the list, as long as there's an item there.
+        """
+        if len(self.st) > 0:
+            return self.st[-1]
+        else:
+            return None
+
+    def size(self):
+        return len(self.st)
+    
+    def is_empty(self):
+        return len(self.st) == 0
+    
+    def __repr__(self):
+        return str(self.st)
+    
+class Queue(object):
+    """Describes a queue that can be used with enqueue
+    and dequeue methods.
+    """
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        return self.queue.pop(0)
+    
+    def size(self):
+        return len(self.queue)
+    
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+    def __repr__(self):
+        return str(self.queue)
+
 class Deque(object):
     """Describes a double-ended queue, with index 0
     the "front" of the Deque and index -1 at the rear.
@@ -113,8 +203,7 @@ class Node(object):
     def get_next(self):
         return self.next
     def __repr__(self):
-        return "Node[data=" + str(self.data) + ",next=" + str(self.next) + 
-"]"
+        return "Node[data=" + str(self.data) + ",next=" + str(self.next) + "]"
 
 class UnorderedList(object):
     """Defines an unordered (unsorted) list of nodes.
@@ -163,12 +252,9 @@ class UnorderedList(object):
         """
         current = self.head
         previous = None
-        while current != None and self.head != None:    # Have to search 
-entire list
-            if current.get_data() == data:              # need to remove 
-it
-                if previous == None:                    # we're at the 
-head
+        while current != None and self.head != None:    # Have to search entire list
+            if current.get_data() == data:              # need to remove it
+                if previous == None:                    # we're at the head
                     self.head = current.get_next()
                     current = current.get_next()
                 else:
@@ -254,8 +340,7 @@ head
             return result
 
     def __repr__(self):
-        """Creates a representation of the list suitable for printing, 
-debugging.
+        """Creates a representation of the list suitable for printing, debugging.
         """
         result = "UnorderedList["
         next_node = self.head
@@ -295,6 +380,71 @@ class UnorderedListStack(object):
 
     def is_empty(self):
         return self.ul.is_empty()
+
+class HashTable(object):
+    
+    def __init__(self, size):
+        self.keys = size * [None]
+        self.data = size * [None]
+        self.size = size
+
+    def put(self, key, value):
+        
+        hash_value = key % self.size
+        while self.keys[hash_value] != None and self.keys[hash_value] != key:
+            hash_value += 1
+        if self.keys[hash_value] == key:
+            self.data[hash_value] = value
+        else:
+            self.keys[hash_value] = key
+            self.data[hash_value] = value
+        
+    def get(self, key):
+        hash_value = key % self.size
+        while self.keys[hash_value] != None and self.keys[hash_value] != key:
+            hash_value += 1
+        if self.keys[hash_value] == key:
+            return self.data[hash_value]
+        else:
+            return None
+        
+    def __str__(self):
+        return "Keys:   " + str(self.keys) + "\n" + \
+               "Values: " + str(self.data)
+    
+class BinaryTree(object):
+
+    def __init__(self, val):
+        self.val = val
+        self.left_child = None
+        self.right_child = None
+        
+    def get_root_val(self):
+        return self.val
+    
+    def set_root_val(self, val):
+        self.val = val
+
+    def get_left_child(self):
+        return self.left_child
+
+    def get_right_child(self):
+        return self.right_child
+
+    def insert_left(self, val):
+        new_subtree = BinaryTree(val)
+        new_subtree.left_child = self.left_child
+        self.left_child = new_subtree
+    
+    def insert_right(self, val):
+        new_subtree = BinaryTree(val)
+        new_subtree.right_child = self.right_child
+        self.right_child = new_subtree
+
+    def __str__(self):
+        return "BinaryTree[key=" + str(self.val) + \
+               ",left_child=" + str(self.left_child) + \
+               ",right_child=" + str(self.right_child) + "]"
     
 def main():
     pass
